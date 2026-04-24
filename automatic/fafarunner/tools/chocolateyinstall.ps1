00ccdaf1ffeeb8f205fd451fa47ca691c8117895
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2026-04-24 02:35:34 +0000

    AU: 1 updated - fafarunner [skip ci] https://gist.github.com/kjxbyz/6e62e5cab8fb28487b265620bd85755d/933dff7ced493778da8077f0882ece187d5bb34b

--- automatic/fafarunner/tools/chocolateyinstall.ps1
+++ automatic/fafarunner/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/fafarunner/fafarunner/releases/download/v2.8.0/fafarunner-2.8.0-windows-setup-x64.exe'
+$url64      = 'https://github.com/fafarunner/fafarunner/releases/download/v2.9.0/fafarunner-2.9.0-windows-setup-x64.exe'
 
 $arch = Get-OSArchitectureWidth -Compare 64
 
@@ -16,7 +16,7 @@ $packageArgs = @{
 
   softwareName  = 'fafarunner*'
 
-  checksum64    = '1ee639e81d0fec8442511e2a95ec2f3d0d9e3fb75bdf9610d29598e74dc47ee8'
+  checksum64    = '01deb21920c259b30563c82a2296c859a6629d7723536c1e497c358422733991'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
